b097235603b36e7e2f619108157775c81d06f7e0
Arpit Singh Patel <apple@Apples-MacBook-Pro.local>
2026-08-30 23:50:19 +0530

    2nd commit in the main branch

--- first.py
+++ first.py
@@ -1,2 +1,4 @@
 print(("Hello, World!" + "\n") *10)
 print((("This is a line in the newsub branch") + "\n") *3)
+print((("This is a line in the other branch") + "\n") *2)
+print((("This is a 2 line in the other branch") + "\n") *5)
