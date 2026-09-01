ef2c1720c08d1da3d47d6cd56e2a451495151965
arpit-aka-appu <apple@Apples-MacBook-Pro.local>
2026-09-01 14:26:52 +0530

    exp &inp

--- conversions.py
+++ conversions.py
@@ -3,3 +3,9 @@ y  = 10.5
 z =  int(x) + float(y)
 print("the valur of z : ",z)
 print("dataype of z :", type(z))
+
+a = input("enter no.")
+b = int(a)
+c = int(a) - 2*b
+d = c-int(a)
+print ("the value of d:",d)
